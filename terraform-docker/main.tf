1e903ce23cec0af2cffd373ebdd8afd976f9fb78
Carlos <solrac.hincapie@gmail.com>
2021-05-21 23:14:55 +0000

    Multiple Resources and Count

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -14,23 +14,51 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  count = 2
+  length = 4
+  special = false
+  upper = false
+}
+
+# resource "random_string" "random2" {
+#   length = 4
+#   special = false
+#   upper = false
+# }
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  #name  = "nodered"
+  count = 2
+  name = join("-",["nodered",random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
 }
 
 output "IP_Address"{
   #value = docker_container.nodered_container.ip_address
   #description = "La dirección ip del contenedor"
-  value = join(":",[docker_container.nodered_container.ip_address,docker_container.nodered_container.ports[0].external])
+  value = join(":",[docker_container.nodered_container[0].ip_address,docker_container.nodered_container[0].ports[0].external])
   description = "La dirección ip y el puerto externo del contenedor"
 }
 
 output "container_name"{
-  value = docker_container.nodered_container.name
+  value = docker_container.nodered_container[0].name
   description = "El nombre del contenedor"
+}
+
+output "IP_Address2"{
+  #value = docker_container.nodered_container.ip_address
+  #description = "La dirección ip del contenedor"
+  value = join(":",[docker_container.nodered_container[1].ip_address,docker_container.nodered_container[1].ports[0].external])
+  description = "La dirección ip y el puerto externo del contenedor"
+}
+
+
+output "container_name2"{
+  value = docker_container.nodered_container[1].name
+  description = "El nombre del contenedor 2"
 }
